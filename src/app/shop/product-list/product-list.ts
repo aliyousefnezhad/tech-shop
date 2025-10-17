32475a4d471be8e03cbf4416476e218751a6f42b
aliyousefnezhad <aliyousefnezhad1372@gmail.com>
2025-10-17 07:02:21 -0700

    Deploy Angular app to GitHub Pages

--- src/app/shop/product-list/product-list.ts
+++ src/app/shop/product-list/product-list.ts
@@ -1,28 +1,46 @@
-// src/app/shop/product-list/product-list.ts
-
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ProductService } from '../../core/services/product';
 import { Product } from '../../core/models/product';
-import { RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
-import { NgIf } from '@angular/common'; // ⬅️ اضافه شد برای رفع خطا *ngIf
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [RouterLink, CurrencyPipe, MatButton, NgIf], // ⬅️ NgIf اضافه شد
+  imports: [CurrencyPipe, MatButton, MatIcon],
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.scss']
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
+  starArray = Array(5);
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    public route: ActivatedRoute 
+  ) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe((products: Product[]) => {
+    // بارگذاری تمام محصولات
+    this.productService.getProducts().subscribe(products => {
       this.products = products;
+      this.applyFilter();
     });
+
+    // شنود تغییرات پارامترهای URL
+    this.route.queryParams.subscribe(params => {
+      this.applyFilter(params['category']);
+    });
+  }
+
+  applyFilter(category?: string) {
+    if (category) {
+      this.filteredProducts = this.products.filter(p => p.category === category);
+    } else {
+      this.filteredProducts = this.products; // همه محصولات
+    }
   }
 }
